16e27cbe57957800fffd474288a0836cbeff3567
James Tang <j@jamztang.com>
2015-06-19 08:57:12 +0800

    Initial Commit

---
+++ Draw/Document.swift
@@ -0,0 +1,34 @@
+//
+//  Document.swift
+//  Draw
+//
+//  Created by James Tang on 19/6/15.
+//  Copyright © 2015 James Tang. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override func windowControllerDidLoadNib(aController: NSWindowController) {
+        super.windowControllerDidLoadNib(aController)
+        // Add any code here that needs to be executed once the windowController has loaded the document's window.
+    }
+
+    override class func autosavesInPlace() -> Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let windowController = storyboard.instantiateControllerWithIdentifier("Document Window Controller") as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+}
---
+++ Draw/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Draw
+//
+//  Created by James Tang on 19/6/15.
+//  Copyright © 2015 James Tang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
